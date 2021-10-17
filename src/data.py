4625032ac54f70e14f59d12599da80527ae8c4e3
AurumnPegasus <subramanian.shivansh@gmail.com>
2021-10-17 11:32:05 +0530

    vwap

--- src/data.py
+++ src/data.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
 from icecream import ic
+import datetime
 
 class Data:
     def __init__(self, tickers):
@@ -16,6 +17,23 @@ class Data:
             threads=True
         )
 
+    def vwap(self, df):
+        cv = 0
+        values = []
+        for index, rows in df.iterrows():
+            current_date = datetime.datetime.strptime(str(index), '%Y-%m-%d %H:%M:%S').weekday()
+            cv += rows['Volume']
+            typical_price = (rows['High'] + rows["Low"] + rows["Close"])/3
+            numerator = typical_price*rows['Volume']
+            if cv == 0:
+                values.append(float('NaN'))
+            else:
+                values.append(numerator/cv)
+            if current_date == 4:
+                cv = 0
+
+        return values
+
     def saveData(self):
         mixed = self.downloadData()
         columns = ['Adj Close', 'Close', 'High', 'Low', 'Open', 'Volume']
@@ -26,7 +44,16 @@ class Data:
             df = pd.DataFrame(data).T
             df.columns = columns
             df.dropna(inplace=True)
+            df['Date'] = df.index
+            df['VWAP'] = self.vwap(df)
+            df.dropna(inplace=True)
             df.to_csv(f"../data/company/{ticker}.csv")
+
+    def getData(self):
+        for ticker in self.tickerlist:
+            df = pd.read_csv(f"../data/company/{ticker}.csv")
+            ic(df)
+            break
 
 d = Data( [
           "ADM", "BAYRY", "BG", "SMG"
@@ -35,4 +62,4 @@ d = Data( [
           "GNRC", "PPSI", "VST" # electronics
 
          ])
-d.saveData()
+d.getData()
